b65c0cbd92cf17fa66216acf636fd9940e80f3f5
shaunsephton <shaunsephton@gmail.com>
2010-09-29 11:51:47 +0200

    always allow user access

--- sharing/utils.py
+++ sharing/utils.py
@@ -5,6 +5,10 @@ def limit_queryset_by_permission(qs, perm, user):
     # TODO: There must be a more efficient way to do this, refactor.
     filtered_object_ids = []
     for obj in qs:
+        # User always has access to herself.
+        if obj == user:
+            filtered_object_ids.append(obj.id)
+            continue
         if user.has_perm(perm, obj):
             filtered_object_ids.append(obj.id)
     return qs.filter(id__in=filtered_object_ids)

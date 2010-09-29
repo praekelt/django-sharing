011381a9422178ed6b6b70ba31e12afa2a221657
shaunsephton <shaunsephton@gmail.com>
2010-09-29 11:52:09 +0200

    test utils

--- sharing/tests.py
+++ sharing/tests.py
@@ -5,17 +5,20 @@ from django.contrib.auth.models import Group,  User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from sharing.admin import ShareAdmin
+from sharing import utils
+from sharing.admin import ShareAdminMixin
 from sharing.models import GroupShare, UserShare
 from snippetscream import RequestFactory
-
 
 class TestModel(models.Model):
     pass
 models.register_models('sharing', TestModel)
 
-class SharingBackendTestCase(unittest.TestCase):
+class TestModelAdmin(ShareAdminMixin, admin.ModelAdmin):
+    pass
+admin.site.register(TestModel, TestModelAdmin)
 
+class ShareBackendTestCase(unittest.TestCase):
     def setUp(self):
         # Create test object, users and groups.
         self.obj = TestModel.objects.create(id=1)
@@ -109,7 +112,7 @@ class ShareAdminTestCase(unittest.TestCase):
         self.group_user.groups.add(self.group)
         self.group_user.save()
         self.request = RequestFactory().get('/')
-        self.share_admin = ShareAdmin(TestModel, admin.site)
+        self.share_admin = TestModelAdmin(TestModel, admin.site)
     
     def tearDown(self):
         # Delete created objects.
@@ -207,3 +210,97 @@ class ShareAdminTestCase(unittest.TestCase):
             object_id=self.obj.id,
         )
         self.failUnless(self.obj in self.share_admin.queryset(self.request))
+
+class UtilsTestCase(unittest.TestCase):
+    def setUp(self):
+        # Create test object, users and groups.
+        self.obj = TestModel.objects.create(id=1)
+        self.user = User.objects.create(username='user')
+        self.group = Group.objects.create(name='group')
+        self.group_user = User.objects.create(username='group_user')
+        self.group_user.groups.add(self.group)
+        self.group_user.save()
+    
+    def tearDown(self):
+        # Delete created objects.
+        self.obj.delete()
+        self.user.delete()
+        self.group.delete()
+        self.group_user.delete()
+
+    def test_limit_queryset_by_permission(self):
+        # Return False if 'view' permission is not found for user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'view', self.user))
+        
+        # Return False if 'change' permission is not found for user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'change', self.user))
+        
+        # Return False if 'delete' permission is not found for user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'delete', self.user))
+        
+        # Return False if 'view' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'view', self.group_user))
+        
+        # Return False if 'change' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'change', self.group_user))
+        
+        # Return False if 'delete' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'delete', self.group_user))
+       
+        # Setup user share.
+        UserShare.objects.create(
+            user=self.user,
+            can_view=True,
+            can_change=True,
+            can_delete=True,
+            content_type=ContentType.objects.get_for_model(self.obj),
+            object_id=self.obj.id,
+        )
+        
+        # Return True if 'view' permission is found for user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'view', self.user))
+        
+        # Return True if 'change' permission is found for user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'change', self.user))
+        
+        # Return True if 'delete' permission is found for user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'delete', self.user))
+       
+        # Group share has not yet been setup, so should all still be unpermitted.
+        # Return False if 'view' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'view', self.group_user))
+        
+        # Return False if 'change' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'change', self.group_user))
+        
+        # Return False if 'delete' permission is not found for group user.
+        self.failIf(utils.limit_queryset_by_permission(TestModel.objects.all(), 'delete', self.group_user))
+        
+        # Setup group share.
+        GroupShare.objects.create(
+            group=self.group,
+            can_view=True,
+            can_change=True,
+            can_delete=True,
+            content_type=ContentType.objects.get_for_model(self.obj),
+            object_id=self.obj.id,
+        )
+        
+        # Return True if 'view' permission is found for group user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'view', self.group_user))
+        
+        # Return True if 'change' permission is found for group user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'change', self.group_user))
+        
+        # Return True if 'delete' permission is found for group user.
+        self.failUnless(utils.limit_queryset_by_permission(TestModel.objects.all(), 'delete', self.group_user))
+
+        # User should always have access to its own user object.
+        self.failUnless(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'view', self.user))
+        self.failUnless(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'change', self.user))
+        self.failUnless(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'delete', self.user))
+        
+        # User should never have access to other user objects.
+        self.failIf(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'view', self.group_user))
+        self.failIf(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'change', self.group_user))
+        self.failIf(self.user in utils.limit_queryset_by_permission(User.objects.all(), 'delete', self.group_user))
